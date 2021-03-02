fc2442273dd3b8bfc3551439de2323eea06eefa8
SimonCieslar <xciesl05@vutbr.cz>
2021-03-02 10:19:56 +0100

    Waveform_Vivado

--- Labs/03-vivado/mux_2bit_4to1/mux_2bit_4to1.srcs/sim_1/new/tb_mux_2bit_4to1.vhd
+++ Labs/03-vivado/mux_2bit_4to1/mux_2bit_4to1.srcs/sim_1/new/tb_mux_2bit_4to1.vhd
@@ -15,14 +15,14 @@ use ieee.std_logic_1164.all;
 ------------------------------------------------------------------------
 -- Entity declaration for testbench
 ------------------------------------------------------------------------
-entity tb_comparator_2bit is
+entity tb_mux_2bit_4to1 is
     -- Entity of testbench is always empty
-end entity tb_comparator_2bit;
+end entity tb_mux_2bit_4to1;
 
 ------------------------------------------------------------------------
 -- Architecture body for testbench
 ------------------------------------------------------------------------
-architecture testbench of tb_comparator_2bit is
+architecture testbench of tb_mux_2bit_4to1 is
 
     -- Local signals
     signal s_a       : std_logic_vector(2 - 1 downto 0);
@@ -53,17 +53,17 @@ begin
         report "Stimulus process started" severity note;
 
 
-        s_d <= "00"; s_c <= "00"; s_b <= "00"; s_a <= "00"; s_sel <= "00"; wait for 100 ns;
+        s_d <= "00"; s_c <= "00"; s_b <= "00"; s_a <= "00";
+        s_sel <= "00"; wait for 100 ns;
         
-        s_a <= "01"; wait for 100 ns;
-        s_b <= "01"; wait for 100 ns;
+        s_d <= "00"; s_c <= "00"; s_b <= "10"; s_a <= "00";
+        s_sel <= "00"; wait for 100 ns;
         
-        s_sel <= "01"; wait for 100 ns;
-        s_c <= "00"; wait for 100 ns;
-        s_b <= "11"; wait for 100 ns;
+        s_d <= "00"; s_c <= "10"; s_b <= "01"; s_a <= "11";
+        s_sel <= "00"; wait for 100 ns;
         
-          s_d <= "10"; s_c <= "11"; s_b <= "01"; s_a <= "00"; 
-          s_sel <= "10"; wait for 100 ns;
+        s_d <= "10"; s_c <= "11"; s_b <= "01"; s_a <= "00"; 
+        s_sel <= "10"; wait for 100 ns;
 
         
         -- Report a note at the end of stimulus process
