4517a84ef9be4f9e73d06d18923ecbff32d600cd
SimonCieslar <xciesl05@vutbr.cz>
2021-03-26 11:07:31 +0100

    Part of the fourth one

--- Labs/07-ffs/flip_flops/flip_flops.srcs/sim_1/new/tb_t_ff_rst.vhd
+++ Labs/07-ffs/flip_flops/flip_flops.srcs/sim_1/new/tb_t_ff_rst.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 24.03.2021 14:25:59
+-- Create Date: 26.03.2021 10:58:08
 -- Design Name: 
--- Module Name: tb_jk_ff_rst - Behavioral
+-- Module Name: tb_t_ff_rst - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -31,33 +31,31 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity tb_jk_ff_rst is
+entity tb_t_ff_rst is
 --  Port ( );
-end tb_jk_ff_rst;
+end tb_t_ff_rst;
 
-architecture Behavioral of tb_jk_ff_rst is
+architecture Behavioral of tb_t_ff_rst is
 
   constant c_CLK_100MHz_PERIOD : time    := 10 ns;
 
   signal s_clk_100MHz    : std_logic;
   
   signal s_rst    : std_logic;
-  signal s_j      : std_logic;
-  signal s_k      : std_logic;
+  signal s_t      : std_logic;
   signal s_q      : std_logic;
   signal s_q_bar  : std_logic;
 
 begin
 
-    uut_jk_ff_rst : entity work.jk_ff_rst
-        port map (
+    uut_t_ff_rst : entity work.t_ff_rst
+    port map (
              clk   => s_clk_100MHz,   
              rst   => s_rst, 
-             j     => s_j,
-             k     => s_k,  
+             t     => s_t,  
              q     => s_q,   
-             q_bar => s_q_bar
-    );
+             q_bar => s_q_bar    
+             );
 
     --------------------------------------------------------------------
     -- Clock generation process
@@ -72,77 +70,25 @@ begin
         end loop;
         wait;
     end process p_clk_gen;
+    
     ------------------------------------------
     --Reset generation process
-    ------------------------------------------     
+    ------------------------------------------    
      p_reset_gen : process
         begin
             s_rst <= '0';
-            wait for 28 ns;
+            wait for 10 ns;
             
             -- Reset activated
             s_rst <= '1';
-            wait for 13 ns;
+            wait for 10 ns;
     
             -- Reset deactivated
             s_rst <= '0';
-            
-            wait for 17 ns;
-            s_rst <= '1';
-            
-            wait for 33 ns;
-            s_rst <= '0';
-            
-            wait for 660 ns;
-            s_rst <= '1';
-    
             wait;
         end process p_reset_gen;
-
+        
     ------------------------------------------
     --Data generation process
-    ------------------------------------------    
-    p_stimulus : process
-    begin
-        report "Stimulus process started" severity note;
-        s_j <= '0';
-        s_k <= '0';
-        
-        --d sekv
-        wait for 37 ns;
-        s_j <= '0';
-        s_k <= '0';        
-        wait for 3 ns;
-        s_j <= '1';
-        s_k <= '0';                       
-        wait for 7 ns;
-        s_j <= '0';
-        s_k <= '1';        
-        wait for 14 ns;
-        s_j <= '1';
-        s_k <= '0';       
-        wait for 7 ns;
-        s_j <= '1';
-        s_k <= '1';
-        --/d sekv
-        
-        --d sekv
-        wait for 7 ns;
-        s_j <= '0';
-        s_k <= '0';             
-        wait for 7 ns;
-        s_j <= '0';
-        s_k <= '1';     
-        wait for 7 ns;
-        s_j <= '1';
-        s_k <= '0';     
-        wait for 7 ns;
-        s_j <= '1';
-        s_k <= '1';
-        --/d sekv
-        
-        report "Stimulus process finished" severity note;
-        wait;
-    end process p_stimulus;
-    
+    ------------------------------------------        
 end Behavioral;
